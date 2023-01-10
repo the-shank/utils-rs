270d6268c2d5be4db063593507831aab2ec7138d
Shank <shashanksharma21@gmail.com>
2023-01-09 23:42:31 -0500

    add todo: add description of the program

--- src/bin/broken_symlinks.rs
+++ src/bin/broken_symlinks.rs
@@ -1,19 +1,19 @@
 use clap::Parser;
-use std::path::PathBuf;
-use std::env;
 use color_eyre::eyre::Result;
+use std::env;
+use std::path::PathBuf;
 
 extern crate utils_rs;
 use utils_rs::common::parsers;
 
 #[derive(Parser, Debug)]
 struct Args {
+    // TODO: add description of the program
     #[arg(value_parser=parsers::parse_dir)]
     root: Option<PathBuf>,
 }
 
-
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let args = Args::parse();
     let root_dir = args.root.unwrap_or(env::current_dir()?);
     dbg!(&root_dir);
